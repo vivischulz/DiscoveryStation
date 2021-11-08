c0b6041b6ba9a06c85a5a69a55d040413e794fd4
Vivi Schulz <haweirh@gmail.com>
2021-11-07 21:06:00 -0800

    add introduction.

--- dist/js.d818e0ef.js
+++ dist/js.d818e0ef.js
@@ -1908,7 +1908,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58093" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60959" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
