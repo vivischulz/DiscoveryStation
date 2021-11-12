ee1c9f59e72bab51f740fce1658a5787761c614d
Vivi Schulz <haweirh@gmail.com>
2021-11-11 16:17:34 -0800

    add about-us page.

--- dist/collapsible.a375820b.js
+++ dist/collapsible.a375820b.js
@@ -152,7 +152,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61182" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63611" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
