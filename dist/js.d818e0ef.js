a3edab75df3f81adc62e5f97cedbf37797ce5f16
Vivi Schulz <haweirh@gmail.com>
2021-11-15 13:34:22 -0800

    create contact form

--- dist/js.d818e0ef.js
+++ dist/js.d818e0ef.js
@@ -1894,7 +1894,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60841" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56981" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
