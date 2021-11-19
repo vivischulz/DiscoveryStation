34cffdeb3bc600510046f358f7fc2e6a2eb2ba59
Vivi Schulz <haweirh@gmail.com>
2021-11-19 14:06:32 -0800

    add scroll effect

---
+++ src/js/scroll.js
@@ -0,0 +1,14 @@
+ // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+ window.onscroll = function() {scrollFunction()};
+  
+ function scrollFunction() {
+   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+     document.getElementById("nav").style.padding = "1rem 3rem";
+     document.getElementById("nav").style.fontSize = "1.8rem";
+     document.getElementById("nav__brand").style.width = "180px";
+   } else {
+     document.getElementById("nav").style.padding= "2rem 3rem";
+     document.getElementById("nav").style.fontSize = "2rem";
+     document.getElementById("nav__brand").style.width = "200px";
+   }
+ }
