1be9bc1aec1c5ea902ae1b8c584ff55ee8216007
Vivi Schulz <haweirh@gmail.com>
2021-11-20 22:12:32 -0800

    complete footer.

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,14 +1,14 @@
- // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+ // When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's width
  window.onscroll = function() {scrollFunction()};
   
  function scrollFunction() {
    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
      document.getElementById("nav").style.padding = "1rem 3rem";
      
-     document.getElementById("nav__brand").style.width = "180px";
+     document.getElementById("nav__brand").style.width = "160px";
    } else {
      document.getElementById("nav").style.padding= "2rem 3rem";
     
-     document.getElementById("nav__brand").style.width = "200px";
+     document.getElementById("nav__brand").style.width = "180px";
    }
  }
