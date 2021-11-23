483bf80aafa222416ef5e62958e0b352720a2e5a
Vivi Schulz <haweirh@gmail.com>
2021-11-22 19:20:47 -0800

    fix link bug.

--- dist/base.48f2b131.js
+++ dist/base.48f2b131.js
@@ -222,7 +222,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63179" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53075" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
